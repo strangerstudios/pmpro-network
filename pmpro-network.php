afaf9c6f77810dfc2f37690fa098d4349c3ce9bf
David Wanjuki <wanjuki@outlook.com>
2026-03-20 17:15:56 +0300

    Move pmpro_network_new_site after error check
    Ensure the pmpro_network_new_site action is only fired when wpmu_create_blog succeeds. The hook was previously triggered before checking for a WP_Error, which could cause the action to run on failed site creation.

--- pmpro-network.php
+++ pmpro-network.php
@@ -430,11 +430,11 @@ function pmpron_addSite( $sitename, $sitetitle, $user_id = null ) {
 	);
 	$blog_id = wpmu_create_blog( $site, $path, $sitetitle, $user->ID, $meta );
 
-	do_action( 'pmpro_network_new_site', $blog_id, $user->ID );
-
 	if ( is_a( $blog_id, 'WP_Error' ) ) {
 		return new WP_Error( 'blogcreate_failed', __( '<strong>ERROR</strong>: Site creation failed.' ) );
 	}
+
+	do_action( 'pmpro_network_new_site', $blog_id, $user->ID );
 
 	// Save array of all blog ids.
 	$blog_ids = pmpron_getBlogsForUser( $user->ID );
